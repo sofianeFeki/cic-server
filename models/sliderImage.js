fb0b7e49ebaff027c44e4a929e6cfe728a35421f
sofianeFeki <backofficekompar@gmail.com>
2024-08-07 12:11:07 +0100

    Update sliderImage.js

--- models/sliderImage.js
+++ models/sliderImage.js
@@ -1,1 +1,11 @@
+const mongoose = require('mongoose');
+
+const sliderImageSchema = new mongoose.Schema({
+  image: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('SliderImage', sliderImageSchema);
 
