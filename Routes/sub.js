13a61e52a4d64017fb1edc195b8b0c7b4e7b58ee
sofianeFeki <backofficekompar@gmail.com>
2024-09-01 22:07:43 +0100

    Update sub.js

--- Routes/sub.js
+++ Routes/sub.js
@@ -1,14 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 // //middlewares
 // const { authCheck, adminCheck } = require('../middlewares/auth');
-// //controller
-// const { create, read, update, remove, list } = require('../Controlles/sub');
+//controller
+const { create, read, update, remove, list } = require('../Controlles/sub');
 
-// router.post('/sub', authCheck, adminCheck, create);
-// router.get('/subs', list);
-// router.get('/sub/:slug', read);
-// router.put('/sub/:slug', authCheck, adminCheck, update);
-// router.delete('/sub/:slug', authCheck, adminCheck, remove);
+router.post('/sub', create);
+router.get('/subs', list);
+router.get('/sub/:slug', read);
+router.put('/sub/:slug', update);
+router.delete('/sub/:slug', remove);
 
 module.exports = router;
