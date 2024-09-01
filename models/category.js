d093a792a991f7fbcf8f6f83b19c6250c6e415cc
sofianeFeki <backofficekompar@gmail.com>
2024-09-01 22:02:35 +0100

    Update category.js

--- models/category.js
+++ models/category.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema(
   {
     name: {
       type: String,
       trim: true,
-      required: "Name is required",
-      minlength: [2, "too short"],
-      maxlenght: [32, "too long"],
+      required: 'Name is required',
+      minlength: [2, 'too short'],
+      maxlenght: [32, 'too long'],
     },
     slug: {
       type: String,
@@ -18,4 +18,6 @@ const categorySchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-module.exports = mongoose.model("category", categorySchema);
+
+// Exporting with a capitalized name 'Category'
+module.exports = mongoose.model('Category', categorySchema);
